6baa22b56620458029baa97dc3d048b3db2d9ee8
Bzhen <benoit-g@hotmail.fr>
2025-04-03 15:35:41 +0100

    Add subscriptions route and tests

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,26 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+// Let's start simple: we always return a 200 OK
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 // We return `Server` on the happy path and we dropped the `async` keyword
 // We have no .await call, so it is not needed anymore.
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
-    // No .await here!
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,8 @@ async fn main() -> Result<(), std::io::Error> {
     // Otherwise call .await on our Server
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
 
+    let port = listener.local_addr().unwrap().port();
+    println!("starting on http://127.0.0.1:{}", port);
+
     run(listener)?.await
 }
